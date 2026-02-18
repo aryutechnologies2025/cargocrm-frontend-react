6e63e021b203ddef52693cccc3e715f683d0dc29
archanaradha95 <archanaradha95@gmail.com>
2026-02-18 11:22:35 +0530

    updated login

--- Mainsource/src/pages/Login.jsx
+++ Mainsource/src/pages/Login.jsx
@@ -4,21 +4,83 @@ import cargo from "../assets/Cargos.svg"
 import { LuUser } from "react-icons/lu";
 import { SlLock } from "react-icons/sl";
 import { useNavigate } from "react-router-dom";
-import medics_logo from "../assets/medics_logo.svg";
+
 import Footer from "../components/Footer";
 import cargoLord from "../assets/cargoLord_logo.png";
+
+import axios from "axios";
+import { API_URL } from "../Config";
+import { useState } from "react";
+import Cookies from "js-cookie";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
+import axiosInstance from "../api/axiosInstance";
 
 const Login = () => {
   let navigate = useNavigate();
 
-  function onCLickLogin() {
-    navigate("/dashboard");
+  const [formData, setFormData] = useState({
+  email: "",
+  password: ""
+});
 
-    window.scrollTo({
-      top: 0,
-      behavior: "instant",
-    });
+const [error, setError] = useState(null);
+
+  // function onCLickLogin() {
+  //   navigate("/dashboard");
+
+  //   window.scrollTo({
+  //     top: 0,
+  //     behavior: "instant",
+  //   });
+  // }
+
+    const onCLickLogin = async (e) => {
+  e.preventDefault();
+  setError(null);
+
+  try {
+   const res = await axiosInstance.post(
+      `${API_URL}/api/auth/login`,
+      formData,
+      
+    );
+
+     if (res.data.success) {
+      localStorage.setItem(
+        "cargouser",
+        JSON.stringify(res.data.user)
+      );
+      localStorage.setItem("admin_token", res.data.token);
+      localStorage.setItem("loginTime", Date.now());
+
+      navigate("/dashboard", { replace: true });
+    }
+  } catch (err) {
+    setError(
+      err.response?.data?.message || "Login failed"
+    );
   }
+};
+
+  const handleChange = (e) => {
+  setFormData({
+    ...formData,
+    [e.target.name]: e.target.value
+  });
+};
+
+  const handleKeyUp = (event) => {
+    setError("");
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword); // Toggle password visibility state
+  };
+const activeClass = "underline font-bold text-blue-600";
+const inactiveClass = "hover:underline";
+
   return (
     <div className="min-h-screen flex flex-col justify-between">
 
@@ -37,26 +99,41 @@ const Login = () => {
             LOGIN
           </p>
 
-          <div className="flex md:w-[450px] gap-3 items-center bg-[#e6f2fa] px-4 py-3 rounded-2xl">
+          <div className="relative flex md:w-[450px] gap-3 items-center bg-[#e6f2fa] px-4 py-3 rounded-2xl">
             <LuUser className="text-2xl text-[#057fc4]" />
             <input
-              type="text"
+              type="email"
+              name="email"
               placeholder="Username"
-              name=""
+              value={formData.email}
+              onChange={handleChange}
               id=""
               className="border-none outline-none bg-transparent text-black placeholder-black"
             />
           </div>
 
-          <div className="flex md:w-[450px] gap-3 items-center bg-[#e6f2fa] px-4 py-3 rounded-2xl">
+          <div className="relative flex md:w-[450px] gap-3 items-center bg-[#e6f2fa] px-4 py-3 rounded-2xl">
             <SlLock className="text-2xl text-[#057fc4]" />
             <input
-              type="password"
+              type={showPassword ? "text" : "password"}
+              value={formData.password}
+              onChange={handleChange}
               placeholder="Password"
-              name=""
+              name="password"
               id=""
+               onKeyUp={handleKeyUp}
               className="border-none outline-none bg-transparent text-black placeholder-black"
             />
+            <span
+                  onClick={togglePasswordVisibility}
+                  className="absolute right-4 cursor-pointer text-gray-600"
+                >
+                  {showPassword ? (
+                    <FaEye className="text-xl" />
+                  ) : (
+                    <FaEyeSlash className="text-xl" />
+                  )}
+                </span>
           </div>
 
           <button
