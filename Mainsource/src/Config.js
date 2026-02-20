6f215d3b35a792cc4df30fdaf935e32e9cdac29e
moni <mohanapriya.072@gmail.com>
2026-02-20 14:38:16 +0530

    Updated Login captcha

--- Mainsource/src/Config.js
+++ Mainsource/src/Config.js
@@ -1,2 +1,4 @@
 // export const API_URL = "http://192.168.0.108:5002";  //archana
 export const API_URL = "https://cargocrm.aryuprojects.com";  
+ export const CAPCHA_URL = "6Lfdxm8sAAAAAM9w3lzL6euMcEcbNigIQrr1oFRB";  //site key(live)
+//  export const CAPCHA_URL = "6Lfdxm8sAAAAAM9w3lzL6euMcEcbNigIQrr1oFRB";  //site key(local)
